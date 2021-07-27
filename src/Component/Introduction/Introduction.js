02d97e70ce81e0b84b4ad705dc8434b038d18e57
Candice Whiskey <candicewhiskey77@gmail.com>
2021-07-27 15:22:50 -0400

    removed link

--- src/Component/Introduction/Introduction.js
+++ src/Component/Introduction/Introduction.js
@@ -11,7 +11,7 @@ export default class Introduction extends Component {
                                         <div className="slider-text-inner js-fullheight">
                                             <div className="desc">
                                                 <h1>Hi, I am Candice! Learn a little about me!</h1>
-                                                <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                                                <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                                             </div>
                                         </div>
                                     </div>
