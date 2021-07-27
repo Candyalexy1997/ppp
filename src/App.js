b0df3405382f10d786ebea4e97ef26b1d401226b
Candice Whiskey <candicewhiskey77@gmail.com>
2021-07-27 15:08:50 -0400

    added some components, no styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+//import Sidebar from './Component/Sidebar/Sidebar'
+import Introduction from './Component/Introduction/Introduction'
+import About from './Component/About/About'
+//import Projects from './Component/Projects/Projects'
+//import Blog from './Component/Blog/Blog'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+	render() {
+		return (
+			<div id="colorlib-page">
+				<div id="container-wrap">
+	
+					<div id="colorlib-main">
+						<Introduction></Introduction>
+						<About></About>
+				
+			
+					</div>
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default App;
+
+
+
